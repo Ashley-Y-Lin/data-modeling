352b692a852eeb961809694520832a3e35b05f5b
Ashley Lin <ashleyylin11@gmail.com>
2023-04-25 17:25:04 -0700

    finish outerspace.sql

--- outerspace.sql
+++ outerspace.sql
@@ -1,26 +1,49 @@
 CREATE TABLE stars (
-  name text PRIMARY KEY,
-  temperature text
+  name TEXT PRIMARY KEY,
+  temperature INT NOT NULL
 );
+-- TODO: make TEXT things VARCHAR
+
+-- TODO: make temperature an int not null
 
 CREATE TABLE moons (
-  name text PRIMARY KEY
+  name TEXT PRIMARY KEY,
+  planet_name VARCHAR(50) NOT NULL REFERENCES planets
 );
+
+-- TODO: remove the planets_to_moons, add a planets to moons
 
 CREATE TABLE planets (
-  id serial PRIMARY KEY,
-  name VARCHAR(50) not null,
-  orbital_period NUMERIC(5, 3) not null,
-  star_name text not null REFERENCES stars,
-  moon_name text REFERENCES moons,
-  unique (name, star_name, moon_name)
+  name VARCHAR(50) PRIMARY KEY,
+  orbital_period NUMERIC(5, 3) NOT NULL,
+  star_name TEXT NOT NULL REFERENCES stars
 );
 
+INSERT INTO stars (name, temperature)
+  VALUES
+    ('The Sun', 5800),
+    ('Proxima Centauri', 3042),
+    ('Gliese 876', 3192);
 
--- INSERT INTO planets
--- (id, name, orbital_period, star_code, moon_code)
---   VALUES (
---     'earth', 1.00, 1
---   )
+INSERT INTO moons (name, planet_name)
+  VALUES
+    ('The Moon', 'Earth'),
+    ('Phobos', 'Mars'),
+    ('Deimos', 'Mars');
 
+INSERT INTO planets (name, orbital_period, star_name)
+  VALUES
+    ('Earth', 1.00, 'The Sun'),
+    ('Mars', 1.882, 'The Sun'),
+    ('Venus', 0.62, 'The Sun'),
+    ('Proxima Centauri b', 0.03, 'Proxima Centauri'),
+    ('Gliese 876 b', 0.236, 'Gliese 876');
 
+SELECT p.name, star_name, COUNT(m.name)
+  FROM planets AS p
+  LEFT OUTER JOIN moons AS m
+    ON p.name = m.planet_name
+  GROUP BY p.name
+  ORDER BY p.name;
+
+-- TODO: be consistent in aliases
